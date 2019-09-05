cc5c894348124a55e35400c227323ab6c6c689c2
Codequeen <54287358+Estherokafor05@users.noreply.github.com>
2019-09-05 02:03:46 +0100

    Esther

---
+++ index.js
@@ -0,0 +1,9 @@
+|function validateform(){
+      
+   var x = document.getElementById('name').value;
+   var y = document.getElementById(;message').value;
+                                
+  if (x.length < 4 || y.length < 20 ) {
+    alert("fill in the necassary information"); return false;
+   }
+}
